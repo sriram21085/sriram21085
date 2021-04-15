28b4baa4594fe2f327c89186daa5f63278df34ce
Ramkumar <ramkumarpearl83@gmail.com>
2021-04-15 13:56:55 +0530

    pro added

--- FormsExample/src/app/app.module.ts
+++ FormsExample/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProducteditComponent } from './product/productedit/productedit.component';
+import { ProductlistComponent } from './product/productlist/productlist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProducteditComponent,
+    ProductlistComponent
   ],
   imports: [
     BrowserModule,
